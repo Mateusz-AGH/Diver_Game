89e84a0b77ead3917538491017084f2363fdb31c
Mateusz-AGH <mateuszkosek123@gmail.com>
2025-01-23 20:18:34 +0100

    First Push

---
+++ Diver.cpp
@@ -0,0 +1,115 @@
+﻿// Diver.cpp: definiuje punkt wejścia dla aplikacji.
+//
+
+#include "Diver.h"
+#include "raylib.h"
+
+using namespace std;
+typedef enum GameScreen {TITLE, GAMEPLAY, ENDING } GameScreen;
+
+int main(void)
+{
+    // Initialization
+    //--------------------------------------------------------------------------------------
+    const int screenWidth = 800;
+    const int screenHeight = 450;
+    Vector2 Diver_Position = { (float)screenWidth / 2, (float)screenHeight / 2 };
+    InitWindow(screenWidth, screenHeight, "Diver");
+
+    GameScreen currentScreen = TITLE;
+
+    // TODO: Initialize all required variables and load all required data here!
+
+    int framesCounter = 0;          // Useful to count frames
+    SetTargetFPS(60);               // Set desired framerate (frames-per-second)
+    //--------------------------------------------------------------------------------------
+
+    // Main game loop
+    while (!WindowShouldClose())    // Detect window close button or ESC key
+    {
+        // Update
+        //----------------------------------------------------------------------------------
+        switch (currentScreen)
+            {
+            case TITLE:
+            {
+                // TODO: Update TITLE screen variables here!
+
+                // Press enter to change to GAMEPLAY screen
+                if (IsKeyPressed(KEY_ENTER))
+                {
+                    currentScreen = GAMEPLAY;
+                }
+            } break;
+            case GAMEPLAY:
+            {
+                if (IsKeyDown(KEY_RIGHT)) Diver_Position.x += 2.0f;
+                if (IsKeyDown(KEY_LEFT)) Diver_Position.x -= 2.0f;
+                if (IsKeyDown(KEY_UP)) Diver_Position.y -= 2.0f;
+                if (IsKeyDown(KEY_DOWN)) Diver_Position.y += 2.0f;
+
+                // Press enter to change to ENDING screen
+                if (IsKeyPressed(KEY_ENTER))
+                {
+                    currentScreen = ENDING;
+                }
+            } break;
+            case ENDING:
+            {
+                // TODO: Update ENDING screen variables here!
+
+                // Press enter to return to TITLE screen
+                if (IsKeyPressed(KEY_ENTER))
+                {
+                    currentScreen = TITLE;
+                }
+            } break;
+            default: break;
+        }
+
+        // Draw
+        //----------------------------------------------------------------------------------
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        switch (currentScreen)
+        {
+        case TITLE:
+        {
+            // TODO: Draw TITLE screen here!
+            DrawRectangle(0, 0, screenWidth, screenHeight, GREEN);
+            DrawText("TITLE SCREEN", 20, 20, 40, DARKGREEN);
+            DrawText("PRESS ENTER TO START", 300, 220, 20, DARKGREEN);
+
+        } break;
+        case GAMEPLAY:
+        {
+            // TODO: Draw GAMEPLAY screen here!
+            DrawRectangle(0, 0, screenWidth, screenHeight, PURPLE);
+            DrawText("DIVE!", 20, 20, 40, MAROON);
+            DrawText("PRESS ENTER TO END", 20, 400, 20, MAROON);
+            DrawCircleV(Diver_Position, 50, MAROON);
+
+        } break;
+        case ENDING:
+        {
+            // TODO: Draw ENDING screen here!
+            DrawRectangle(0, 0, screenWidth, screenHeight, BLUE);
+            DrawText("ENDING SCREEN", 20, 20, 40, DARKBLUE);
+            DrawText("PRESS ENTER", 300, 220, 20, DARKBLUE);
+
+        } break;
+        default: break;
+        }
+
+        EndDrawing();
+    }
+
+    // TODO: Unload all loaded data (textures, fonts, audio) here!
+
+    CloseWindow();        // Close window and OpenGL context
+    //--------------------------------------------------------------------------------------
+
+    return 0;
+}
